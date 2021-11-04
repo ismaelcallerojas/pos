670bb28027a119ec788d5d36fbb2ba558f9487b2
ismael <ismaelcallerojas@gmail.com>
2021-11-04 11:58:27 -0400

    Todas las vistas del sistema

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2021 <a href="#">Sistema Web de Ventas y Control de Stock - MARALIB OFFICE SRL</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
